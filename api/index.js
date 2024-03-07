be0d087942c15ba54e035a304abba65079560807
Lucas Quadros <1lucascq@gmail.com.br>
2024-03-07 13:14:03 -0300

    fix: enable cors through vercel.json

--- api/index.js
+++ api/index.js
@@ -6,14 +6,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowCors = (req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-	next();
-};
-
-app.use(allowCors);
 app.use(bodyParser.json());
 app.use("/api", routes);
 
