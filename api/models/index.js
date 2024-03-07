bae64cd98b30559f1b98b68f12886d84c1b9604f
Lucas Quadros <1lucascq@gmail.com.br>
2024-03-07 09:12:11 -0300

    debug: import pg as dialectModule

--- api/models/index.js
+++ api/models/index.js
@@ -1,8 +1,9 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-
+const pg = require("pg")
 const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
 	dialect: "postgres",
+    dialectModule: pg,
 	host: process.env.POSTGRES_HOST,
 	port: 5432,
 	dialectOptions: {
