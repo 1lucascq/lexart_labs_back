73e9b4a136129f12166867dfb4a82a4c72955e82
Lucas Quadros <1lucascq@gmail.com.br>
2024-03-07 09:00:24 -0300

    debug: another approach to fix the problem with pg

--- api/models/index.js
+++ api/models/index.js
@@ -1,17 +1,16 @@
 const Sequelize = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
 	dialect: "postgres",
-    dialectModule: "pg",
 	host: process.env.POSTGRES_HOST,
 	port: 5432,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 const User = require("./users")(sequelize, Sequelize);
@@ -20,5 +19,5 @@ const Smartphone = require("./smartphones")(sequelize, Sequelize);
 module.exports = {
 	User,
 	Smartphone,
-	sequelize
+	sequelize,
 };
